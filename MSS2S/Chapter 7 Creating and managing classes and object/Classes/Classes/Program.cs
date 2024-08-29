2c6581b2ada6283c95bfbd85d1f45339a331ba3e
Edwdard <hua_zhang2002@msn.com>
2024-08-29 21:01:03 +0800

    chapter 7 update
    chapter 7 update

---
+++ MSS2S/Chapter 7 Creating and managing classes and object/Classes/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+     class Program
+    {
+        static void doWork()
+        {
+            var origin = new Point(0, 0);
+            var bottomRight = new Point(4, 5);
+            double distance = origin.DistanceTo(bottomRight);
+            Console.WriteLine($"Distance is : {distance}");
+            Console.WriteLine($"Number of Point Object: {Point.ObjectCount()}");
+
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
