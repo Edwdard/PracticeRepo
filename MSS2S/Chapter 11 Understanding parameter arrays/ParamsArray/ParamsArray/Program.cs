ce2f9c543f26f1672cdc0836817d445b086e6c91
Edwdard <hua_zhang2002@msn.com>
2024-11-16 16:02:51 +0800

    chapter11 updated

---
+++ MSS2S/Chapter 11 Understanding parameter arrays/ParamsArray/ParamsArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+
+
+    internal class Program
+    {
+
+        static void doWork()
+        {
+            Console.WriteLine(Utils.Sum(10,9,8));
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+
+            }
+        }
+    }
+}
