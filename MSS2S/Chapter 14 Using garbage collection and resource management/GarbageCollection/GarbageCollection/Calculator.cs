5ef4abc2cda7c9db39c85baede9891bc9c2a6692
Edwdard <125868300+Edwdard@users.noreply.github.com>
2024-12-04 16:59:34 +0800

    Chapter 14 updated

---
+++ MSS2S/Chapter 14 Using garbage collection and resource management/GarbageCollection/GarbageCollection/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollection
+{
+    class Calculator : IDisposable
+    {
+        private bool disposed = false;
+
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created.");
+        }
+
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator being finalized");
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (!this.disposed)
+            {
+                Console.WriteLine("Calculator being disposed");
+            }
+            this.disposed = true;   
+            GC.SuppressFinalize(this);
+        }
+
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+
+
+    }
+    
+          
+}
---
+++ MSS2S/Chapter 14 Using garbage collection and resource management/GarbageCollection/GarbageCollection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollection
+{
+    internal class Program
+    { 
+    
+        static void doWork()
+        {
+            using (Calculator calculator = new Calculator())
+            {
+                Console.WriteLine($"120 / 15 = {calculator.Divide(120, 0)}");
+            }
+            Console.WriteLine("Program finishing");
+            
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (InvalidOperationException ex)
+            {
+                 Console.WriteLine(ex.Message);
+            }
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+        }
+    }
+}
