e8068d1574e5142e4c35e7721541533e6d0def4b
Edwdard <125868300+Edwdard@users.noreply.github.com>
2025-01-17 17:28:15 +0800

    chapter 15 update 2

---
+++ MSS2S/Chapter 15 Implementing properties to access fields/StudentEnrolment/Entrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrolment
+{
+    //internal class Entrollment
+    //{
+    //}
+    record Enrollment(int StudentID, string CourseName, DateOnly DateEnrolled);
+
+}
---
+++ MSS2S/Chapter 15 Implementing properties to access fields/StudentEnrolment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StudentEnrolment
+{
+    class Program
+    {
+        static void doWork()
+        {
+            var Enrollments = new Enrollment[4];
+            Enrollments[0] = new Enrollment(StudentID:1,CourseName:"Physics",DateEnrolled: new DateOnly(2021,07,20));
+            Enrollments[1] = new Enrollment(StudentID: 1, CourseName: "Chemistry", DateEnrolled: new DateOnly(2021, 07, 20));
+            Enrollments[2] =  Enrollments[0];
+            Enrollments[3] =  Enrollments[0] with { StudentID = 2 };
+
+            foreach(var Enrollment in Enrollments)
+            {
+                Console.WriteLine($"{Enrollment}");
+            }
+
+            var firstEnrollment= Enrollments[0];
+            foreach(var Enrollment in Enrollments)
+            {
+                Console.WriteLine($"{firstEnrollment==Enrollment}");
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
