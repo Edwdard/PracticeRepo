832f15ff9d63c067870e86beb350d62f0c16e77a
adalexe <139778687+adalexe@users.noreply.github.com>
2025-04-21 17:15:55 +0800

    Final Update for chapter 19

--- MSS2S/Chapter 19 Enumerating collections/IteratorBinaryTree/BinaryTree/Tree.cs
+++ MSS2S/Chapter 19 Enumerating collections/IteratorBinaryTree/BinaryTree/Tree.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace BinaryTree
 {
-    public class Tree<TItem> :IEnumerable<TItem> where TItem : IComparable<TItem>
+    public class Tree<TItem> : IEnumerable<TItem> where TItem : IComparable<TItem>
     {
         public TItem NodeData { get; set; }
-        public Tree<TItem> LeftTree { get; set; }
-        public Tree<TItem> RightTree { get; set; }
+        public Tree<TItem>? LeftTree { get; set; }
+        public Tree<TItem>? RightTree { get; set; }
 
         public Tree(TItem nodeValue)
         {
@@ -23,9 +21,10 @@ namespace BinaryTree
         public void Insert(TItem newItem)
         {
             TItem currentNodeValue = this.NodeData;
+
             if (currentNodeValue.CompareTo(newItem) > 0)
             {
-                // Insert the item into the left subtree
+                // Insert the new item into the left subtree
                 if (this.LeftTree is null)
                 {
                     this.LeftTree = new Tree<TItem>(newItem);
@@ -58,18 +57,18 @@ namespace BinaryTree
                 result = this.LeftTree.WalkTree();
             }
 
-            result += String.Format($" {this.NodeData.ToString()} ");
+            result += $" {this.NodeData.ToString()} ";
 
             if (this.RightTree is not null)
             {
                 result += this.RightTree.WalkTree();
             }
-
             return result;
         }
 
         IEnumerator<TItem> IEnumerable<TItem>.GetEnumerator()
         {
+            //throw new NotImplementedException();
             if (this.LeftTree is not null)
             {
                 foreach (TItem item in this.LeftTree)
@@ -77,16 +76,16 @@ namespace BinaryTree
                     yield return item;
                 }
             }
-
             yield return this.NodeData;
-
+            
             if (this.RightTree is not null)
             {
-                foreach (TItem item in this.RightTree)
+                foreach(TItem item in this.RightTree)
                 {
                     yield return item;
                 }
             }
+
         }
 
         IEnumerator IEnumerable.GetEnumerator()
